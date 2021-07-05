39d15824f7cf6759cf210c14fc396a10aa558cbd
Alex <alexchenchen1@gmail.com>
2021-07-04 17:07:08 -0700

    Generator.py finished

--- generator.py
+++ generator.py
@@ -1,13 +1,25 @@
+from io import BytesIO
 import multiprocessing as mp
-import subprocess
-import csv
+import pycurl
 
 processes_array = []
 
-def runner(game):
-    print("done")
+def fetcher(games):
+    cURL = pycurl.Curl()
+    e = BytesIO()
+    cURL.setopt(cURL.WRITEDATA, e)
 
-def process_creator(url_location, children=4):
+    for game_url in games:
+        cURL.setopt(cURL.URL, "https://www.basketball-reference.com{}".format(game_url))
+        cURL.perform()
+
+        with open("{}".format(game_url.rsplit('/', 1)[-1]), "wb+") as output_file:
+            output_file.write(e.getbuffer())
+
+        e.flush()
+
+
+def process_creator(children=4):
     # Load all our urls into a list
     with open("schedule_scraper/urls.txt") as url_file:
         urls = url_file.read().split()
@@ -15,15 +27,15 @@ def process_creator(url_location, children=4):
     # Split this list into evenly sized chunks for allocating to pools
     url_arrays = [[] for x in range(0, children)]
     for index in range(children):
-        url_arrays[index] = urls[int(len(urls)/children*index):int(len(urls)/children*(index+1))]
+        url_arrays[index] = [urls[int(len(urls)/children*index):int(len(urls)/children*(index+1))]]
     
     m = mp.Manager()
     with mp.Pool(processes=children) as pool:
         for index in range(children):
-            processes_array.append(pool.apply_async(runner, args=(urls_array[index])))
+            processes_array.append(pool.apply_async(fetcher, args=(url_arrays[index])))
 
-        for process:
-            processes_array.get()
+        for process in processes_array:
+            process.get()
 
 if __name__ == '__main__':
-    process_create(sys.argv[1], sys.argv[2])
+    process_creator()
